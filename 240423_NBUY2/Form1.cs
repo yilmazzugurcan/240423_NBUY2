0a8eddbfc89655953058cae330ee2a988896c4b6
yilma <yilma@DESKTOP-U1LH654>
2023-04-24 21:29:35 +0300

    Add project files.

---
+++ 240423_NBUY2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _240423_NBUY2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlConnection baglanti = new SqlConnection();
+            //server=localhost veya . konulursa pcdeki servera baglanır.
+            baglanti.ConnectionString = "Server=localhost;Database=Northwind;User=sa;Pwd=123";
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "Select * from Urunler";
+            cmd.Connection=baglanti;
+            baglanti.Open();
+            SqlDataReader reader=cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string adi = reader["UrunAdi"].ToString();
+                string fiyat = reader["BirimFiyati"].ToString();
+                string stok = reader["HedefStokDuzeyi"].ToString();
+                LstUrunler.Items.Add(string.Format("{0}-{1}-{2}", adi, fiyat, stok));
+
+            }
+        }
+    }
+}
